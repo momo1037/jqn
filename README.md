37705ecfa61678dade00bab1a78e4a4f66eb865d
w72 <w72e@qq.com>
2022-10-25 22:28:07 +0800

    feat: use eval

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 ```bash
 # use npm
-echo '{"a": {"b": 1}}' | npx jqn 'a.b'
+echo '{"a":{"b":1}}' | npx jqn '.a.b'
 # out => 1
 
 # or install globally
 npm i -g jqn
-echo '{"a": {"b": 1}}' | jqn 'a.b'
-# out => 1
+echo '{"a":{"b":1}}' | jqn '.a.b'
+# output => 1
+
+# support js array functions
+echo '{"a":{"b":[1,2,3,4]}}' | jqn '.a.b.filter(v=>v>2)[0]'
+# output => 3
 ```
--- main.js
+++ main.js
@@ -10,11 +10,5 @@ stdin.on("data", function (chunk) {
 
 stdin.on("end", function () {
   const [path] = process.argv.slice(2);
-  const object = JSON.parse(data);
-  const value = path
-    .replace(/\[(\d+)\]/g, ".$1")
-    .split(".")
-    .reduce((obj, cur) => Object(obj)[cur], object);
-  const ret = String(value ?? "");
-  console.log(ret);
+  console.log(eval(`(${data})${path}`));
 });
